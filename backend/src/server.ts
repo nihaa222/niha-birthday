4e62cfe6228a9196c0efbc790b040af521d12200
nihaawww <debniharika3@gmail.com>
2026-06-02 17:58:54 +0530

    added process.exit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,10 +25,20 @@ const connectDB = async () => {
     }
 }
 
-connectDB()
+
 
 app.use("/birthdayData", birthdayRoutes)
 
-app.listen(PORT, ()=> {
-   console.log(`Server running on port ${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
